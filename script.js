af2f0e9b11ed512377a702eb572c6325f92e238c
D4YonSoundcloud <mattd4y@gmail.com>
2020-02-29 20:58:03 -0500

    Final changes 8:57 added generated text 2-29

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function inputChanged(e) {
     e.target.value
   );
 };
+
+let box = document.querySelector('#box')
+let boxStyle = window.getComputedStyle(box);
+let boxShadow = boxStyle.getPropertyValue('box-shadow');
+console.log(boxShadow);
+
+document.querySelector('#submit').addEventListener('click', onSubmit)
+
+function onSubmit (e) {
+    let cssText = document.createElement('p');
+    cssText.innerHTML = `box-shadow: ${boxShadow};`;
+    let sideBar = document.querySelector('#cssText');
+    sideBar.appendChild(cssText)
+}
