d6f86283cf821b84ffffd30fa781ed7d62e4af44
D4YonSoundcloud <mattd4y@gmail.com>
2020-03-02 13:57:42 -0500

    1;57 pm Bug fixed, need to make repons 3-2-20

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelectorAll('input').forEach(input => {
-  input.addEventListener('input', inputChanged);
+document.querySelectorAll("input").forEach(input => {
+  input.addEventListener("input", inputChanged);
 });
 
 function inputChanged(e) {
@@ -8,23 +8,22 @@ function inputChanged(e) {
     `--${e.target.name}`,
     e.target.value
   );
-};
+}
 
-let box = document.querySelector('#box')
-let boxStyle = window.getComputedStyle(box);
-let boxShadow = boxStyle.getPropertyValue('box-shadow');
-let boxColor = boxStyle.getPropertyValue('--box-color');
-console.log(boxShadow);
-console.log(boxColor);
+document.querySelector("#submit").addEventListener("click", onSubmit);
 
-document.querySelector('#submit').addEventListener('click', onSubmit)
-
-function onSubmit (e) {
-    let cssBoxShadowText = document.createElement('p');
-    cssBoxShadowText.innerHTML = `box-shadow: ${boxShadow};`;
-    let cssBoxColorText = document.createElement('p');
-    cssBoxColorText.innerHTML = `background-color: ${boxColor};`;
-    let cssTextDiv = document.querySelector('#cssText');
-    cssTextDiv.appendChild(cssBoxShadowText);
-    cssTextDiv.appendChild(cssBoxColorText);
+function onSubmit(e) {
+  let box = document.querySelector("#box");
+  let boxStyle = window.getComputedStyle(box);
+  let boxShadow = boxStyle.getPropertyValue("box-shadow");
+  let boxColor = boxStyle.getPropertyValue("--box-color");
+  console.log(boxShadow);
+  console.log(boxColor);
+  let cssBoxShadowText = document.createElement("p");
+  cssBoxShadowText.innerHTML = `box-shadow: ${boxShadow};`;
+  let cssBoxColorText = document.createElement("p");
+  cssBoxColorText.innerHTML = `background-color: ${boxColor};`;
+  let cssTextDiv = document.querySelector("#cssText");
+  cssTextDiv.appendChild(cssBoxShadowText);
+  cssTextDiv.appendChild(cssBoxColorText);
 }
