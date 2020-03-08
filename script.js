a850e9a89e4d9f698dae08b9b345ec27c23d6f06
D4YonSoundcloud <mattd4y@gmail.com>
2020-03-08 13:02:34 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll("input").forEach(input => {
+  input.addEventListener("input", inputChanged);
+});
+
+function inputChanged(e) {
+  console.log("it worked");
+  document.documentElement.style.setProperty(
+    `--${e.target.name}`,
+    e.target.value
+  );
+}
+
+document.querySelector("#submit").addEventListener("click", onSubmit);
+
+function onSubmit(e) {
+  let box = document.querySelector("#box");
+  let boxStyle = window.getComputedStyle(box);
+  let boxShadow = boxStyle.getPropertyValue("box-shadow");
+  let boxColor = boxStyle.getPropertyValue("--box-color");
+  console.log(boxShadow);
+  console.log(boxColor);
+  let cssBoxShadowText = document.createElement("p");
+  cssBoxShadowText.innerHTML = `box-shadow: ${boxShadow};`;
+  let cssBoxColorText = document.createElement("p");
+  cssBoxColorText.innerHTML = `background-color: ${boxColor};`;
+  let cssTextDiv = document.querySelector("#cssText");
+  cssTextDiv.appendChild(cssBoxShadowText);
+  cssTextDiv.appendChild(cssBoxColorText);
+}
